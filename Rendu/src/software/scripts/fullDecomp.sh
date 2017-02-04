406e4b7e1ba9990bc50c8f845d04c6c9a13d1cac
florentinGo <florentin.gonthier@gmail.com>
2017-02-04 13:55:42 +0100

    Delete fullDecomp.sh

--- Rendu/src/software/scripts/fullDecomp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# TEST de chaque niveau de compression avec décompression complète
-for x in {1..9};
-do
-	./bin/exec $x 0 so_useless.pgm fullDecomp_${x}.pgm
-done
-
-rm so_useless.pgm
